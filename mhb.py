4f64f9aabb253d2fd328250f8584694e170fff71
592k <kweonok94@gmail.com>
2022-05-09 21:26:42 +0900

    feat: Created Monty Hall Problem

---
+++ mhb.py
@@ -0,0 +1,14 @@
+import random
+li = [0,1,2]
+n1 = 0
+n2 = 0
+for i in range(1000):
+  ran = random.sample(li,3)
+  u = ran[0]
+  m = ran[1]
+  if u == 2:
+    n1 = n1+1
+  elif u != 2 and m != 2:
+    n2 = n2+1
+print(n1/1000)
+print(n2/1000)
