8d11c60f942d5d7f5763a3d9c47ad392aaabc7ee
syoh5188 <syoh5188@gmail.com>
2022-05-11 09:25:31 +0900

    Review codes, running python file, check the result -> Task list Done

--- mhb.py
+++ mhb.py
@@ -1,3 +1,5 @@
+# code reviewed at 11-05-2022 09:24 by.syoh5188
+
 import random
 li = [0,1,2]
 n1 = 0
@@ -12,3 +14,5 @@ for i in range(1000):
     n2 = n2+1
 print(n1/1000)
 print(n2/1000)
+
+# result : 0.312, 0.362 result checked!! 
